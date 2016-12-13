12c1e55e08578841d94492aa582937dcc5da577f
shezjl <418079061@qq.com>
2016-12-13 18:07:19 +0800

    [DEV,PROTO] 1.protobuf hello 测试

---
+++ protobuf-hello/doc.go
@@ -0,0 +1,6 @@
+// protobuf-hello project doc.go
+
+/*
+protobuf-hello document
+*/
+package main
---
+++ protobuf-hello/main.go
@@ -0,0 +1,35 @@
+// protobuf-hello project main.go
+package main
+
+import (
+	"log"
+
+	"golang-project/proto/example"
+
+	"github.com/golang/protobuf/proto"
+)
+
+func main() {
+	test := &example.Test{
+		Label: proto.String("hello"),
+		Type:  proto.Int32(17),
+		Reps:  []int64{1, 2, 3},
+		Optionalgroup: &example.Test_OptionalGroup{
+			RequiredField: proto.String("good bye"),
+		},
+	}
+	data, err := proto.Marshal(test)
+	if err != nil {
+		log.Fatal("marshaling error: ", err)
+	}
+	newTest := &example.Test{}
+	err = proto.Unmarshal(data, newTest)
+	if err != nil {
+		log.Fatal("unmarshaling error: ", err)
+	}
+	// Now test and newTest contain the same data.
+	if test.GetLabel() != newTest.GetLabel() {
+		log.Fatalf("data mismatch %q != %q", test.GetLabel(), newTest.GetLabel())
+	}
+	// etc.
+}
