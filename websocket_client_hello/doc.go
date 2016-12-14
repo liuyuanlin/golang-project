61b597a6eb2c2e0d1c1a21f25a0389087202bcd7
shezjl <418079061@qq.com>
2016-12-14 11:43:05 +0800

    [DEV,Websocket] 1.添加websocket hello 测试

---
+++ websocket_client_hello/doc.go
@@ -0,0 +1,6 @@
+// websocket_client_hello project doc.go
+
+/*
+websocket_client_hello document
+*/
+package main
---
+++ websocket_server_hello/doc.go
@@ -0,0 +1,6 @@
+// websocket_server_hello project doc.go
+
+/*
+websocket_server_hello document
+*/
+package main
